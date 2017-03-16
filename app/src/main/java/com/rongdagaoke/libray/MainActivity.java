2dd85197e23aab2b76ca59441d74417c4d9e27e8
l16426434 <l16426434@gmail.com>
2017-03-16 11:55:57 +0800

    new

--- app/src/main/java/com/rongdagaoke/libray/MainActivity.java
+++ app/src/main/java/com/rongdagaoke/libray/MainActivity.java
@@ -1,13 +1,9 @@
 package com.rongdagaoke.libray;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends BaseActivity {
+    
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getContentView() {
+        return R.layout.activity_main;
     }
 }
